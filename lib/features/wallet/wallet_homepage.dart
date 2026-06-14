b80c79a819cf8875536c7c3eeb2439de8620d780
hiofadlikaakbar <hioakbar1@gmail.com>
2026-06-14 16:20:01 +0700

    rombak wallet home

--- lib/features/wallet/wallet_homepage.dart
+++ lib/features/wallet/wallet_homepage.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../auth/login_page.dart';
 
 class WalletHomePage extends StatelessWidget {
   const WalletHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Wallet Home",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8F9FB),
+
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('wallet_users')
+            .doc(user!.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || !snapshot.data!.exists) {
+            return const Center(child: Text("Data wallet belum tersedia"));
+          }
+
+          final data = snapshot.data!.data() as Map<String, dynamic>;
+
+          final balance = data['balance'] ?? 0;
+
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+          
+                  
+                   
+
+               
+            ),
+          );
+        },
       ),
     );
   }
